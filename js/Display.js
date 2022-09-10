90fbb0338662f40c623053f11eaf8417fc8c3bbf
Didier Guerrero <dfguerrero@unicesar.edu.co>
2022-09-10 14:51:22 -0500

    Commit-funcionando
    Revisar textos faltantes, para acomodar esa parte.

--- js/Display.js
+++ js/Display.js
@@ -1,10 +1,54 @@
 //Contructor
+class Display{
+    constructor(displayValorAnterior, displayValorActual){
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();
+        this.tipoOperacion = undefined;
+        this.valorActual= '';
+        this.valorAnterior = '';
+        this.signos = {
+            sumar: '+',
+            restar: '-',
+            multiplicar: 'x',
+            dividir: '%',
+        }
+    }
 
-//calculos
+    borrar(){
+        this.valorActual = this.valorActual.toString().slice(0, -1);
+        this.imprimirValores();
+    }
 
-//agregarNumero
+    borrarTodo(){
+        this.valorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
 
-//imprimaValores
+    computar(tipo) {
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
 
-//Computar 
+    agregarNumero(numero) {
+            if(numero === '.' && this.valorActual.includes('.')) return 
+            this.valorActual = this.valorActual.toString() + numero.toString();
+        }
 
+        imprimirValores(){
+            this.displayValorActual.textContent = this.valorActual;
+            this.displayValorAnterior.textContent = `${this.valorAnterior} ${this.signos[this.tipoOperacion] || ''}`;
+        }
+
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const valorActual = parseFloat(this.valorActual);
+
+        if (isNaN(valorActual) || isNaN(valorAnterior) ) return this.valorActual = this.calculador[this.tipoOperacion](valorAnterior, valorAnterior);
+    }
+}
